ca5d37670285e92f4cb148c977c27c18184bc028
allendefelix <54599121+allendefelix@users.noreply.github.com>
2021-09-28 20:35:46 -0300

    scripts para la creacion de la BD CsvImporter y la tabla Stocks.

---
+++ 02 - Create Table Stocks.sql
@@ -0,0 +1,16 @@
+
+/****** Object:  Table [dbo].[AccountMaster]    Script Date: 28/1/2021 17:57:00 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Stocks](
+	[PointOfSale] [varchar](20) NULL,
+	[Product] [varchar](20) NULL,
+	[Date] [datetime] NULL,
+	[Stock] [int] NULL)
+GO
+
+
