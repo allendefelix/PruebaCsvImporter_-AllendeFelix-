d0777bf998e66503a966a4e862ef04e6f2fcba63
allendefelix <54599121+allendefelix@users.noreply.github.com>
2021-09-28 20:49:01 -0300

    Update 02 - Create Table Stocks.sql

--- 02 - Create Table Stocks.sql
+++ 02 - Create Table Stocks.sql
@@ -1,11 +1,4 @@
 
-/****** Object:  Table [dbo].[AccountMaster]    Script Date: 28/1/2021 17:57:00 ******/
-SET ANSI_NULLS ON
-GO
-
-SET QUOTED_IDENTIFIER ON
-GO
-
 CREATE TABLE [dbo].[Stocks](
 	[PointOfSale] [varchar](20) NULL,
 	[Product] [varchar](20) NULL,
